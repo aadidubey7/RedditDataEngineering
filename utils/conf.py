3b3edad5bc4259d155dd2fb6164f3b49edc069aa
aadidubey7 <aadidubey7@gmail.com>
2025-02-26 15:56:31 +0530

    Added config file

---
+++ utils/conf.py
@@ -0,0 +1,26 @@
+REDDIT_CLIENT_ID = '{REDDIT_CLIENT_ID}',
+REDDIT_CLIENT_SECRET = '{REDDIT_CLIENT_SECRET}',
+REDDIT_USER_AGENT = '{REDDIT_USER_AGENT}'
+
+OUTPUT_PATH = 'data'
+
+AWS_ACCESS_KEY = "{AWS_ACCESS_KEY}"
+AWS_SECRET_KEY = "{AWS_SECRET_KEY}"
+AWS_S3_BUCKET = "{AWS_S3_BUCKET}"
+AWS_REGION = "{AWS_REGION}"
+
+POST_FIELDS = (
+    'id',
+    'title',
+    # 'selftext',
+    'score',
+    'num_comments',
+    'author',
+    'created_utc',
+    'url',
+    # 'upvote_ratio',
+    'over_18',
+    'edited',
+    'spoiler',
+    'stickied'
+)
